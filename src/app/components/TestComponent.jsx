cd9756f900ddb04631ea9b9285aae92df4b40ec8
Franki09 <fgomezrosell@gmail.com>
2025-08-27 18:06:40 -0300

    Inicializacion del proyecto y configuracion para que funcione con Next y TW

---
+++ src/app/components/TestComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TestComponent() {
+  return <div className="bg-red-500">Esto es un test</div>;
+}
+
+export default TestComponent;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        stone: {
+          100: "hsl(30, 54%, 90%)",
+          150: "hsl(30, 18%, 87%)",
+          600: "hsl(30, 10%, 34%)",
+          900: "hsl(24, 5%, 18%)",
+        },
+
+        brown: "hsl(14, 45%, 36%)",
+
+        rose: {
+          800: "hsl(332, 51%, 32%)",
+          50: "hsl(330, 100%, 98%)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
